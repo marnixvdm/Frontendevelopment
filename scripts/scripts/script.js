ab74664c843defa46ed95b5109efb3d34516f386
marnixvdm <147240074+marnixvdm@users.noreply.github.com>
2023-12-11 21:32:36 +0100

    Add files via upload

---
+++ scripts/scripts/script.js
@@ -0,0 +1,18 @@
+
+var openButton = document.querySelector("header > button");
+
+openButton.onclick = openMenu;
+
+function openMenu() {  
+  var deNav = document.querySelector("nav");
+  deNav.classList.add("toonMenu");
+}
+
+var sluitButton = document.querySelector("nav button");
+
+sluitButton.onclick = sluitMenu;
+
+function sluitMenu() {
+  var deNav = document.querySelector("nav");
+  deNav.classList.remove("toonMenu");
+}
